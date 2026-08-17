98ddb5ef7ef5a3a5d182bcb97b0ef491e70dc59e
AntiquusDierum <adw@watson-clan.net>
2026-08-17 20:01:23 +0100

    Add independent watchdog recovery

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,6 +21,7 @@
 #include "adc.h"
 #include "dma.h"
 #include "i2c.h"
+#include "iwdg.h"
 #include "rtc.h"
 #include "tim.h"
 #include "usart.h"
@@ -237,6 +238,14 @@ int main(void)
   Dashboard_Refresh(debug_uart);
 
   StatusLed_SetState(STATUS_LED_NORMAL);
+
+  /*
+   * Start the independent watchdog only after
+   * application initialisation has completed.
+   */
+
+  MX_IWDG_Init();
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -392,6 +401,11 @@ int main(void)
 
 	  StatusLed_Task();
 
+	  if (HAL_IWDG_Refresh(&hiwdg) != HAL_OK)
+	  {
+	      Error_Handler();
+	  }
+
   }
   /* USER CODE END 3 */
 }
@@ -413,10 +427,12 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI
+                              |RCC_OSCILLATORTYPE_LSE;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
